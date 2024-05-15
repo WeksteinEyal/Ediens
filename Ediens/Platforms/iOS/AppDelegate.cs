dce6d7c86a90872af00f37d245d7a4d1dbe563ed
Wekstein <eyalwekstein@outlook.fr>
2024-05-15 16:16:27 +0200

    Ajoutez des fichiers projet.

---
+++ Ediens/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Ediens
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
