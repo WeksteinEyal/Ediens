a314f0c1accd2698eb8a0f66eef77cfa6c7aa9b3
Wekstein <eyalwekstein@outlook.fr>
2024-05-15 17:36:10 +0200

    Most basic form

--- Ediens/MainPage.xaml.cs
+++ Ediens/MainPage.xaml.cs
@@ -1,25 +1,43 @@
-﻿namespace Ediens
+﻿using Microsoft.Maui.Controls;
+
+namespace Ediens
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+        float dailyIntake = 0;
+        string strDailyIntake = "0";
 
         public MainPage()
         {
             InitializeComponent();
+            strDailyIntake = Preferences.Default.Get("DailyIntake", "0");
+            DailyHistory.Text = Preferences.Default.Get("DailyHistory", "");
+            dailyIntake = float.Parse(strDailyIntake);
+            labelDailyIntake.Text = $"Daily Intake: {dailyIntake} kcal";
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        void OnEntryFoodChanged(object sender, TextChangedEventArgs e)
         {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            string oldText = e.OldTextValue;
+            string newText = e.NewTextValue;
+            string myText = entryFood.Text;
         }
+
+        void AddDailyIntake(object sender, EventArgs e)
+        {
+            string foodName = entryFood.Text;
+            float foodQty = float.Parse(entryQty.Text);
+            float foodKcal = float.Parse(entryKcal.Text);
+
+            float intake = foodQty * foodKcal / 100;
+            dailyIntake += intake;
+            labelDailyIntake.Text = $"Daily Intake: {dailyIntake} kcal";
+            DailyHistory.Text = $"{DailyHistory.Text}\n{foodName}, {foodQty}g : {intake}kcal";
+            Preferences.Default.Set("DailyIntake", dailyIntake.ToString());
+            Preferences.Default.Set("DailyHistory", DailyHistory.Text);
+        }
+
+
     }
 
 }
